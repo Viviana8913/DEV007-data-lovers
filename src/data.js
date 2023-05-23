bdfc2a267a0bcfb6443c0282dd1e17a4983fc378
Viviana8913 <mvgomez.alonso@gmail.com>
2023-05-23 15:41:39 -0500

    OH Vivie

--- src/data.js
+++ src/data.js
@@ -1,9 +1,30 @@
-// estas funciones son de ejemplo
+import dataGhibli from "./data/ghibli/ghibli.js"
 
-export const example = () => {
-  return 'example';
-};
+export const filtrarDirector = (nombre) => {
+    return dataGhibli.films.filter((movie) => movie.director === nombre);
+}
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+export const filtrarProductor = (nombre) => {
+    return dataGhibli.films.filter((movie) => movie.producer === nombre);
+}
+
+export const AZdata = () => {
+    const azArray = Array.from(dataGhibli.films);
+    return azArray.sort((nombre, nombre1) => {
+        return (nombre.tittle< nombre1.tittle) ? -1:1
+
+    });
+
+}
+
+export const ZAdata = () => {
+    const zaArray = Array.from(dataGhibli.films);
+    return zaArray.sort((nombre, nombre1) => {
+        return (nombre.tittle< nombre1.tittle) ? 1:-1
+
+    });
+
+}
+
+
+
