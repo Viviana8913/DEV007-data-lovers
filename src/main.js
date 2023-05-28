692154169961b8d15cf46ca4cfbd6a13a6a530a7
Viviana8913 <mvgomez.alonso@gmail.com>
2023-05-28 16:52:19 -0500

    no sirven los test

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { filtrarDirector, filtrarProductor} from './data.js';
+import { AZdata, ZAdata, filtrarDirector, filtrarProductor, filtroBuscar} from './data.js';
 import dataGhibli from './data/ghibli/ghibli.js';
 //console.log(dataGhibli.films);
 const todoGhibli = Object.values(dataGhibli.films);
@@ -13,26 +13,30 @@ const selectDirector = document.getElementById("filtroDirectores");
 const selectProductor = document.getElementById("filtroProductores");
 
 const peliculas = todoGhibli.filter((element) => {
-    return element.title;
+  return element.title;
 });
 const personajes = todoGhibli.filter((element) => {
-    return element.people;
+  return element.people;
 })
 const locaciones = todoGhibli.filter((element) => {
-    return element.locations;
+  return element.locations;
 });
 const vehiculos = todoGhibli.filter((element) => {
-     return element.vehicles;
+  return element.vehicles;
 });
 
 
 botonPeliculas.addEventListener("click", function () {
-    //selectDirector.style.display="none";
-    //selectProductor.style.display="none";
-    //selectOrdenar.style.display="none";
-    contenedorBusqueda.innerHTML = "";
-    peliculas.forEach((film) => {        
-            contenedorBusqueda.innerHTML +=`
+  //selectDirector.style.display="none";
+  //selectProductor.style.display="none";
+  //selectOrdenar.style.display="none";
+  mostrarPeliculas(peliculas);
+});
+
+function mostrarPeliculas (pelis){
+  contenedorBusqueda.innerHTML = "";
+  pelis.forEach((film) => {        
+    contenedorBusqueda.innerHTML +=`
             <div class = "afiche">
             <h2><center>${film.title}</h2>
             <img src="${film.poster}"><br>
@@ -43,14 +47,14 @@ botonPeliculas.addEventListener("click", function () {
             <b style="color:;">Release date:</b> ${film.release_date}
             </div>
             `;       
-    });
-});
-
+  });
+}
+ 
 botonPersonajes.addEventListener("click", function () {
-    contenedorBusqueda.innerHTML = "";
-    personajes.forEach((film) => {
-        film.people.forEach(people => {
-        contenedorBusqueda.innerHTML += `
+  contenedorBusqueda.innerHTML = "";
+  personajes.forEach((film) => {
+    film.people.forEach(people => {
+      contenedorBusqueda.innerHTML += `
          <div class='afiche'>
          <img src='${people.img}' alt='${people.img}'></img>
          <p style="color:purple;">${people.name}</p>
@@ -61,16 +65,16 @@ botonPersonajes.addEventListener("click", function () {
          </div>
          `;
 
-        })
+    })
                        
-    });
+  });
 });
 
 botonLocaciones.addEventListener("click", function () {
-    contenedorBusqueda.innerHTML = "";
-    locaciones.forEach((film) => {
-        film.locations.forEach(location =>{
-            contenedorBusqueda.innerHTML +=
+  contenedorBusqueda.innerHTML = "";
+  locaciones.forEach((film) => {
+    film.locations.forEach(location =>{
+      contenedorBusqueda.innerHTML +=
             `<div class='afiche'>
              <img src='${location.img}'>
              <p style="color:purple;">${location.name}</p>
@@ -81,14 +85,14 @@ botonLocaciones.addEventListener("click", function () {
              </div>
              `;
 
-        })
-    });
+    })
+  });
 });
 botonVehiculos.addEventListener("click", function () {
-    contenedorBusqueda.innerHTML = "";
-    vehiculos.forEach((film) => {
-        film.vehicles.forEach(vehicles => {
-            contenedorBusqueda.innerHTML += `
+  contenedorBusqueda.innerHTML = "";
+  vehiculos.forEach((film) => {
+    film.vehicles.forEach(vehicles => {
+      contenedorBusqueda.innerHTML += `
             <div class='afiche'>
              <img src='${vehicles.img}'>
              <p style="color:purple;">${vehicles.name}</p>
@@ -97,24 +101,55 @@ botonVehiculos.addEventListener("click", function () {
              Pilot : ${vehicles.pilot.name} </p>
              </div>
             `;
-        })        
-    });
+    })        
+  });
 });
 
 selectDirector.addEventListener('change', () => {
-      let seleccionDir= selectDirector.value;      
-      let resultDir= filtrarDirector(seleccionDir);
-      return (resultDir);
+  const seleccionDir= selectDirector.value;      
+  const resultDir= filtrarDirector(seleccionDir);
+  mostrarPeliculas (resultDir);
 });
 
 selectProductor.addEventListener('change', () => {
-      let seleccionPro= selectProductor.value;      
-      let resultPro= filtrarProductor(seleccionPro);
-      return (resultPro);
+  const seleccionPro= selectProductor.value;      
+  const resultPro= filtrarProductor(seleccionPro);
+  mostrarPeliculas (resultPro);
 });
 
+selectOrdenar.addEventListener('change', () => {
+  const optionSelec = selectOrdenar.value;
+  let resultFinal = "";
+  switch (optionSelec){
+  case 'az' :
+    resultFinal = AZdata();
+    break;
+  case 'za':    
+    resultFinal = ZAdata();
+    break;
+  default:
+    break;
+  }
+  mostrarPeliculas(resultFinal);
+})
 
+selectBuscar.addEventListener('keyup', (e) => {
+ const buscarPeli = filtroBuscar(e.target.value,movies);
+ buscarPeli.forEach((film) => {        
+    contenedorBusqueda.innerHTML +=`
+            <div class = "afiche">
+            <h2><center>${film.title}</h2>
+            <img src="${film.poster}"><br>
+            <h3 style="color:#7B21AB;"><center>_______________________________</h3>
+            <h4 style="color:puple;">Synopsis:</h4>${film.description}<br><br>
+            <b style="color:purple;">Director:</b> ${film.director}<br><br>
+            <b style="color:purple;">Producer:</b> ${film.producer}<br><br>
+            <b style="color:;">Release date:</b> ${film.release_date}
+            </div>
+            `;       
+  });
+    
+})
 
-data.js
 
 export default dataGhibli;
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,50 @@
-import { example, anotherExample } from '../src/data.js';
+import { filtrarDirector, filtrarProductor, AZdata, ZAdata } from '../src/data.js';
+import dataGhibli from '../src/data/ghibli/ghibli.js'
 
-
-describe('example', () => {
+// ------- filtrar por director -------//
+describe('filtrarDirector', () => {
   it('is a function', () => {
-    expect(typeof example).toBe('function');
+    expect(typeof filtrarDirector).toBe('function');
   });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+  it('returns `returns movies by director Hayao Miyazaki`', () => {
+    expect(filtrarDirector('Hayao Miyazaki')).toEqual([{'director': 'Hayao Miyazaki', 'title': 'Castle in the Sky'}]);
   });
 });
 
-
-describe('anotherExample', () => {
+// ------- filtrar por productor -------//
+describe('filtrarProductor', () => {
   it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
+    expect(typeof filtrarProductor).toBe('function');
   });
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
+  it('returns `peliculas del productor Toshio Suzuki`', () => {
+    expect(filtrarProductor('Toshio Suzuki')).toEqual([{'producer': 'Toshio Suzuki', 'title': 'Porco Rosso'}]);
   });
+});
+
+//------- orden de AZ -------//
+describe('AZdata', () =>{
+  it('is a function', () => {
+    expect(typeof AZdata).toBe('function');
+  });
+
+  it('ordenando de la A a la Z, el primer elemento debe ser `Castle in the Sky`', () => {
+    const result = AZdata(dataGhibli, 'Castle in the Sky');
+    expect(result[0].title).toBe('Castle in the Sky');
+  });
+
+});
+
+//------- orden de ZA -------//
+describe('ZAdata', () =>{
+  it('is a function', () => {
+    expect(typeof ZAdata).toBe('function');
+  });
+
+  it('ordenando de la Z a la A, el primer elemento debe ser `Whisper of the Heart`', () => {
+    const result = ZAdata(dataGhibli, 'Whisper of the Heart');
+    expect(result[0].title).toBe('Whisper of the Heart');
+  });
+
 });
